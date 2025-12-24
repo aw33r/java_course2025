02f943f69e4be10224d4f958b6669017099801fe
aw33r <fazatronka@mail.ru>
2025-12-24 20:01:57 +0500

    timus tasks

---
+++ timus_tasks/task_2012/main.java
@@ -0,0 +1,27 @@
+package timus_tasks.task_2012;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class main {
+    static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+
+        int numberOfTusks = in.nextInt();
+
+        int difference = 12 - numberOfTusks;
+
+        int time = difference * 45;
+
+        if(time <=240){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+
+        out.flush();
+
+    }
+}
